8fc40e2a0acbfe1647682aea15a2d775d8bedcd2
Peter Salomonsson <Petersalomonsson8@hotmail.com>
2015-09-17 21:59:57 +0200

    Update Dog.cs

--- Dog.cs
+++ Dog.cs
@@ -4,48 +4,77 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestOOPAnimal.Classes
+namespace MH_Animal_Applikation_Upg1.Animals
 {
     class Dog : Mammals
     {
-
-        //protected string _sound = "WoFF";
-        //public int _teeth;
+         private string _sound = "Woff";
 
 
-        public Dog(string name, string age, string gender, string teeth)
-            : base(name, age, gender, teeth)
-        {
-            //bool isNum = int.TryParse(teeth, out validInteger);
+         public Dog(int id, string name, int age, string gender, int teeth, int tail)
+             : base(id, name, age, gender, teeth, tail)
+         {
+             Sound = _sound;
+         }
 
-            Sound = "Woff";
-            //Sound = _sound;
-
-            //Teeth = validInteger;
-        }
-
-        //public override string Sound
+        ///// <summary>
+        ///// The copy constructor
+        ///// </summary>
+        ///// <param name="other"></param>
+        //public Dog(Dog other)
         //{
-        //    get
-        //    {
-        //        return this._sound;
-        //    }
-        //    set
-        //    {
-        //        this._sound = "WoFF";
-        //    }
+        //    //this.EType = other.EType;
+        //    this.Teeth = other.Teeth;
+        //    this.Id = other.Id;
+        //    //this.PostAddress = new Creature(other.PostAddress);
+
+        //    // Then copy the special data for this estate
         //}
 
-        //public override int Teeth
+        /// <summary>
+        /// Copy the common data from a estate object
+        /// </summary>
+        /// <param name="other"></param>
+        public Dog(Animal other)
+        {
+            this.gender = other.gender;
+            this.name = other.name;
+            this.age = other.age;
+            this.Id = other.Id;
+            this.teeth = other.teeth;
+            this.tail = other.tail;
+            //this.teeth = other.teeth;
+            //this.Sound = other.Sound;
+            //this.PostAddress = new Creature(other.PostAddress);
+        }
+
+        public string Sound
+        {
+            get
+            {
+                return this._sound;
+            }
+            set
+            {
+                this._sound = value;
+            }
+        }
+
+
+
+         public override string ToString()
+         {
+             return base.ToString() + " Ljud:" + Sound;
+         }
+
+        //public override string ToString()
         //{
-        //    get
-        //    {
-        //        return this._teeth;
-        //    }
-        //    set
-        //    {
-        //        this._teeth = value;
-        //    }
+        //    //Vhat is {0, -12}, {3, 6} eller {4} ?
+        //    string strOut = String.Format(" , Namn:{0} Ålder: {1},  Kön:{2}, Antal tänder:{3},  Svanslängd:{4}, Ljud: {5}", 
+        //        this.name, this.age, this.gender, this.teeth,this.tail);
+
+        //    strOut = strOut.ToUpper();
+        //    return strOut;
         //}
     }
 }
