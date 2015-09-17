9a74eb34ad03292faf6e5e216020c0eeac849752
Peter Salomonsson <Petersalomonsson8@hotmail.com>
2015-09-17 22:00:54 +0200

    Update Mammals.cs

--- Mammals.cs
+++ Mammals.cs
@@ -4,38 +4,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestOOPAnimal.Classes
+namespace MH_Animal_Applikation_Upg1.Animals
 {
     class Mammals : Animal
     {
-
         //DESSA BEHÖVS!??!
         //string _teeth;
         //string _sound;
         private int validInteger;
+        //Teeth and tail make it not working.
+        //public int teeth;
+        //public int tail;
         //public int Teeth { get; protected set; }
 
-
-        public Mammals(string name, string age, string gender, string teeth)
-            : base(name, age, gender)
+        public Mammals()
         {
-            bool isNum = int.TryParse(teeth, out validInteger);
-            this.Teeth = validInteger;
+            ////bool isNum = int.TryParse(teeth, out validInteger);
+        
         }
 
-        //public override int Teeth
+        public Mammals(int id, string name, int age, string gender, int _teeth, int _tail)
+            : base(id, name, age, gender)
+        {
+            ////bool isNum = int.TryParse(teeth, out validInteger);
+
+            Teeth = _teeth;
+            //Tail = _tail;
+        }
+
+        //public Mammals(Animal other)
         //{
-        //    get
-        //    {
-        //        return this._teeth;
-        //    }
-        //    set
-        //    {
-        //        this._teeth = value;
-        //    }
+        //    this.gender = other.gender;
+        //    this.name = other.name;
+        //    this.age = other.age;
+        //    this.Id = other.Id;
+        //    //this.teeth = other.teeth;
+        //    this.tail = other.tail;
+        //    //this.Sound = other.Sound;
+        //    //this.PostAddress = new Creature(other.PostAddress);
         //}
 
-        //public override string Sound
+        public int Teeth
+        {
+            get
+            {
+                return this.teeth;
+            }
+            set
+            {
+                this.teeth = value;
+            }
+        }
+        public int Tail
+        {
+            get
+            {
+                return this.tail;
+            }
+            set
+            {
+                this.tail = value;
+            }
+        }
+
+        //public Mammals(Animal other)
+        //{
+        //    this.name = other.name;
+        //    this.age = other.age;
+        //    this.Id = other.Id;
+        //    this.teeth = other.teeth;
+        //    //this.Sound = other.Sound;
+        //    //this.PostAddress = new Creature(other.PostAddress);
+        //}
+
+        //public virtual string Sound
         //{
         //    get
         //    {
@@ -46,9 +88,9 @@ namespace TestOOPAnimal.Classes
         //        this._sound = value;
         //    }
         //}
-
-
-
-
+        public override string ToString()
+        {
+            return base.ToString() + " Antal tänder:"+ Teeth + " Svanslängd:" + Tail;
+        }
     }
 }
