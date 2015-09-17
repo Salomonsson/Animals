538a58087f85636a5a2f8ce055aa7889c5e32f7f
Peter Salomonsson <Petersalomonsson8@hotmail.com>
2015-09-17 21:59:05 +0200

    Update Cat.cs

--- Cat.cs
+++ Cat.cs
@@ -4,28 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TestOOPAnimal.Classes
+namespace MH_Animal_Applikation_Upg1.Animals
 {
-    class cat : Animal
+    class Cat :  Mammals
     {
-        private string _sound = "Mjaaaau";
+         private string _sound = "Mjaaaau";
 
 
-        public cat(string name, string age, string gender, string teeth)
-            : base(name, age, gender, teeth)
-        {
-            Sound = _sound;
-        }
+         public Cat(int id, string name, int age, string gender, int teeth, int tail)
+             : base(id, name, age, gender, teeth, tail)
+         {
+             Sound = _sound;
+         }
+         public Cat(Animal other)
+         {
+             this.gender = other.gender;
+             this.name = other.name;
+             this.age = other.age;
+             this.Id = other.Id;
+             this.teeth = other.teeth;
+             this.tail = other.tail;
+             //this.Sound = other.Sound;
+             //this.PostAddress = new Creature(other.PostAddress);
+         }
+        //public Cat(Mammals other)
+        //{
 
-        public override string Sound
+        //    this.teeth = other.teeth;
+        //    this.tail = other.tail;
+        //    //this.Sound = other.Sound;
+        //    //this.PostAddress = new Creature(other.PostAddress);
+        //}
+        public string Sound
         {
             get
             {
                 return this._sound;
             }
+            set
+            {
+                this._sound = value;
+            }
         }
 
+        //public override string ToString()
+        //{
+        //    //Vhat is {0, -12}, {3, 6} eller {4} ?
+        //    string strOut = String.Format(" , Namn:{0} Ålder: {1},  Kön:{2}, Antal tänder:{3},  Svanslängd:{4}, Ljud: {5}",
+        //        name, age, this.gender, this.teeth, this.tail, Sound);
 
+        //    strOut = strOut.ToUpper();
+        //    return strOut;
+        //}
 
+        public override string ToString()
+        {
+            return base.ToString() + " Ljud:" + Sound;
+        }
     }
 }
