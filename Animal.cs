c17d4c37f0d2b8f84f5551697ac3bdf2ffb79302
Peter Salomonsson <Petersalomonsson8@hotmail.com>
2015-09-20 15:54:52 +0200

    Update Animal.cs

--- Animal.cs
+++ Animal.cs
@@ -12,14 +12,26 @@ namespace MH_Animal_Applikation_Upg1.Animals
         /// <summary>
         /// Fields
         /// </summary>
-        public int id;
-        public string name;
-        public string gender;
-        public int age;
-        //teeth and tail is måste för att fungera, but i DISLIKE.
+        /// 
+
+        //public AnimalTypes animalType;
+       
+        protected int id;
+        protected string name;
+        protected string gender;
+        protected int age;
+
+        //teeth, tail och speed är ett måste för att fungera, but i DISLIKE.
         public int teeth;
-        public int tail;
+        public double tail;
+        public double speed;
         protected string _sound = "Djur kan låta";
+
+
+        //private EstateTypes MammalsType;
+        //public Creature address;   //"has " relation
+        //public string _sound = "Djur kan låta";
+        //public EstateManager manager;
         #endregion
 
         /// <summary>
@@ -28,32 +40,70 @@ namespace MH_Animal_Applikation_Upg1.Animals
         /// </summary>
         public Animal()
         {
-            
+
+
         }
 
         /// <summary>
         /// Default construktor - a good place for creating 
         /// all the fields in the class
         /// </summary>
-        public Animal(int id, string name, int age, string gender)
+        /// Jag kan ha en constructor som inte tar några inparametrar? 
+        //Jag har rört ihop det lite, jag vet inte om jag behöver constructor med inparametrar. 
+        //Jag tar emot data med hjälp av mina properties. Det är lack of knowledge från min sida. då jag är ny att
+        //använda mig av copy constructors som jag använder i varje instans av objekt. Det är nått som inte stämmer,
+        //Vilket gör mig lite fundersam. Jag försöker få min kod så snygg som möjligt. 
+        public Animal(int id, string name, int age)
         {
             //address = new Creature();
             //teeth;  //m is short for "money". 
             Id = id;
             Name = name;
             Age = age;
-            Gender = gender;
+            //Gender = gender;
 
         }
 
+
+        //Parameters to capture and protect the data inside of the object.
         #region properties
-        //protected internal int Id { get; protected set; }
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
-        //public virtual int Teeth { get; set; }
-        //public virtual int Tail { get; set; }
+        public int Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                    name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+               if (value > 0)
+                   age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get { return gender; }
+            set
+            {
+                gender = value;
+            }
+        }
+
         #endregion
 
 
@@ -66,8 +116,7 @@ namespace MH_Animal_Applikation_Upg1.Animals
         /// <returns>The formatted string.</returns>
         public override string ToString()
         {
-            //Vhat is {0, -12}, {3, 6} eller {4} ?
-            string strOut = String.Format("  ID:{0}, Namn: {1}, Ålder:{2}, Kön: {3}   ", this.id, this.name, this.age, this.gender);
+            string strOut = String.Format("  ID:{0}, Namn: {1}, Ålder:{2}, Kön: {3}   ", Id, Name, Age, Gender);
 
             strOut = strOut.ToUpper();
             return strOut;
