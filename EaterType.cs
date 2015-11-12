7f95cb6763d5313154780f962ad0b0236c0a9e5a
Peter Salomonsson <Petersalomonsson8@hotmail.com>
2015-11-12 17:47:21 +0100

    Create EaterType.cs

---
+++ EaterType.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MH_Animal_Applikation_Upg1
+{
+    public class EaterTypes
+    {
+        public enum EaterType
+        {
+            Carnivora,
+            Herbivore,
+            Omnivorous
+        }
+
+
+    }
+}
