ec535f380f609252321e71dbdb2a38ad81e93ac7
Peter Salomonsson <Petersalomonsson8@hotmail.com>
2015-11-12 17:44:34 +0100

    Update KookaBurra.cs

--- KookaBurra.cs
+++ KookaBurra.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace MH_Animal_Applikation_Upg1.Animals
 {
-    class Kookaburra : Bird
+    public class Kookaburra : Bird
     {
 
         public Kookaburra()
         {
-
+            //Instantiate foodSchedule
+            //myFood.Add("09:00 Bananer");
+            //myFood.Add("11:00 Spindlar och småkryp");
+            //myFood.Add("14:00 Gecko ödla och kackerlackor");
         }
         public Kookaburra(int id, string name, int age, double speed)
              : base(id, name, age, speed)
@@ -27,6 +30,25 @@ namespace MH_Animal_Applikation_Upg1.Animals
 
              //Not protected - BAD, really BAD!
              this.speed = other.speed;
+
+             //Instantiate foodSchedule
+             myFood.Add("09:00 Bananer");
+             myFood.Add("11:00 Spindlar och småkryp");
+             myFood.Add("14:00 Gecko ödla och kackerlackor");
+         }
+
+         public override FoodSchedule GetFoodschedule() { return myFood; }
+
+         public override string IsGoodFor()
+         {
+             //return EaterTypes.EaterType.Carnivora;
+             return EaterTypes.EaterType.Omnivorous.ToString();
+             //return (eater == EaterType.Carnivora) || (eater == EaterType.Omnivorous);
+         }
+
+         public override string GetSpecies()
+         {
+             return AnimalTypes.BirdType.Kookaburra.ToString();
          }
 
          public override string Sound
