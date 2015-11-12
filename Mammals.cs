63a74ed5edb1cd44897ac2a9fca4f4ce8655f5ca
Peter Salomonsson <Petersalomonsson8@hotmail.com>
2015-11-12 17:45:01 +0100

    Update Mammals.cs

--- Mammals.cs
+++ Mammals.cs
@@ -6,12 +6,14 @@ using System.Threading.Tasks;
 
 namespace MH_Animal_Applikation_Upg1.Animals
 {
-    class Mammals : Animal
+    public abstract class Mammals : Animal
     {
         //Teeth and tail make it not working.
         //public int teeth;
         //public double tail;
-
+        //public List<string> m_ingredient = new List<string>();
+        //public string[] _elements = new string[3];
+        
 
         //Default constructor
         public Mammals()
@@ -27,23 +29,6 @@ namespace MH_Animal_Applikation_Upg1.Animals
             Teeth = teeth;
             Tail = tail;
         }
-
-
-        //Enligt mitt tänk så bör en hierarkisk copy-constructor kunna göra jobbet, så dem ärver ned till varandra.
-        //Detta utan resultat. Nu fungerar det så att varje ToString metod i varje klass arver från underklassen
-        //och där efter visas korrekt data. Nackdel i detta är att mina variablar måste finnas i basklassen Animal
-        //med publika encapsulation, VILKET SKA UNDVIKAS. Dem får dock protected set i properties Teeth och Tail under.
-
-        //public Mammals(Animal other)
-        //{
-        //    this.gender = other.gender;
-        //    this.name = other.name;
-        //    this.age = other.age;
-        //    this.Id = other.Id;
-        //    this.teeth = other.teeth;
-        //    this.tail = other.tail;
-        //}
-
 
         //Properties Teeth och Tail har protected set för att skydda objektdata. Dock har jag problemet att instansiera
         //dessa properties. 
@@ -70,10 +55,68 @@ namespace MH_Animal_Applikation_Upg1.Animals
             }
         }
 
+
+
+        /// <summary>
+        /// GetSchedule returnerar en list till string. Vilket är rätt korkat för att sedan ska
+        /// denna string in i en annan lista från FoodSchedule. uhm? Why. 
+        /// Om jag kanske har olika matscheman i FoodSchedule och gör en compare med vilken species, för att
+        /// sedan returnera en list från den klassen, det skulle nog vara mer logiskt.
+        /// </summary>
+        /// <returns>return a string of list animal_foodList</returns>
+        //public override string getSchedule()
+        //{
+        //    //Set an counter
+        //    int counter = 0;
+           
+        //    StringBuilder textOut = new StringBuilder();
+        //    //Get named species
+        //    textOut.Append(this.getSpecies()).Append(" (");
+
+        //    //For each add into list, make as a string, then return
+        //    foreach (string s in this.Elements)
+        //    {
+        //        counter++;
+        //        //textOut.Append(counter).Append(",").Append(s).Append("\n");
+        //        textOut.AppendLine().Append(counter).Append(",").Append(s).AppendLine();
+        //        //test += s.ToString() + counter + "\r\n";
+
+        //    }
+
+        //    textOut.Append(")");
+        //    //remove the last comma 
+        //    //return textOut.ToString().Remove(textOut.ToString().LastIndexOf(","), 1);
+        //    return textOut.ToString();
+        //    //return test;
+        //    //return "Endast Hamburgare tack, jag är en katt!!";
+        //    //return m_ingredient;
+        //}
+
+
+        //public FoodSchedule getFoodSchedule() { return myFood; }
+
+        //public override string getSchedule()
+        //{
+
+        //    foreach (string s in this.Elements)
+        //    {
+        //        test += "Måltid:" + s + "-  ";
+        //    }
+
+        //    return test;
+        //    //return m_ingredient;
+        //}
+
+
+        //public override string[] Elements
+        //{
+        //    get { return _elements; }
+        //}
+
        //Override the base toString, plus add extra info
-        public override string ToString()
-        {
-            return base.ToString() + " Antal tänder:"+ Teeth + " Svanslängd:" + Tail;
-        }
+        //public override string ToString()
+        //{
+        //    return base.ToString() + " Antal tänder:"+ Teeth + " Svanslängd:" + Tail;
+        //}
     }
 }
